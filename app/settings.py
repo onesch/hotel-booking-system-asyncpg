9971e7e8ed3ee68efd42d4fba091ee40d15035c7
onesch <vladimiravakov12345@gmail.com>
2026-09-17 17:08:08 +0400

    style(settings): lint settings.py

--- app/settings.py
+++ app/settings.py
@@ -9,9 +9,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
 
 # Redis
+
 REDIS_URL = os.getenv("REDIS_URL")
 TEST_REDIS_URL = os.getenv("TEST_REDIS_URL")
 
 # Rate limiting
+
 MAX_REQUESTS: int = 10
 WINDOW_SECONDS: int = 60
